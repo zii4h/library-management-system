a21491d89474c84c7cb7cac26fb4ed5fb83e209c
Sophia <sophiakeziahgpineda@gmail.com>
2026-03-21 15:07:48 +0800

    feat: implement classes and subprograms
    class: Database, Book, BorrowedBook

--- OPAC-Ruby/opac_main.rb
+++ OPAC-Ruby/opac_main.rb
@@ -5,17 +5,75 @@
 # Framework: Sinatra (Web-based GUI)
 # -------------------------------------------------------------------
 
-ENV['MARIADB_TLS_DISABLE_PEER_VERIFICATION'] = '1' #ssl
+ENV['MARIADB_TLS_DISABLE_PEER_VERIFICATION'] = '1' #ignore (ssl fix)
 
 DB_CONFIG = {
   host:     'localhost',
   username: 'root',
   password: '',
-  database: 'opac_db' #xampp default db name
+  database: 'opac_db' 
 }.freeze
 
 set :port, 4567 
 set :bind, 'localhost'
 set :server, 'webrick'
 
+
+OVERDUE_FEE_PER_DAY = 10 
+ADMIN_PASSWORD      = 'admin123'  
+
+
+#class: database 
+#purpose: manages mysql connection and table setup
+class Database
+  @connection = nil
+
+  #subprogram: connect
+  #purpose: opens mysql connection and table setup
+  def self.connect
+    @connection = Mysql2::Client.new(DB_CONFIG)
+    setup_tables
+    @connection
+  rescue Mysql2::Error => e
+    puts "[DB ERROR] #{e.message}"
+    exit
+  end
+
+
+#subprogram: setup_tables
+#purpose: creates opac_db, books_table, borrowed_books tables if they don't exist
+
+
+
+#class: Book 
+#purpose: represents a single book record from Books table
+class Book 
+  attr_accessor: 
+
+  def initialize()
+
+  end
+end
+
+
+#class: BorrowedBook
+#purpose: represents borrowing transaction from Borrwed_Books table + overdue calculation
+class BorrowedBook
+  attr_accessor 
+
+  def initialize()
+  end
+
+  #subprogram: overdue_info
+  #purpose: date library. calculates if book is overdue + returns info
+  def overdue_info
+    return {} 
+
+     if days_overdue > 0
+      { overdue: true, days: days_overdue, fee: (days_overdue * OVERDUE_FEE_PER_DAY).to_f }
+    else
+      { overdue: false, days: 0, fee: 0.00 }
+    end
+  end
+end
 
